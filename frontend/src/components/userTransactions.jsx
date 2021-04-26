4ee546c5063ddfeefe48a5415b376157ce8fee30
Sagnik Chowdhury <sagnik.chowdhury@sjsu.edu>
2021-04-26 08:10:37 +0000

    Transactions page reflects actual name, and account balance of selected account
    Transactions page automatically defaults to an available account, if the user doesn't select it via the accounts page

--- frontend/src/components/userTransactions.jsx
+++ frontend/src/components/userTransactions.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { getTransactionHistory } from "../services/historyService";
+import { getUserDetails } from "../services/userService";
+import auth from "../services/authService";
 import "../App.css";
 
 class UserTransactions extends Component {
   constructor(props) {
     super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
     this.state = { //state is by default an object
-       users: [
+	activeAccount : 0,
+	accountBalance : 0,
+        transactions: [
           { Date: '04-13-2021', Description: 'What is Lorem Ipsum Lorem Ipsum is simply dummy text', Amount: '200$', AvailableBalance: '120$' },
           { Date: '04-13-2021', Description: 'What is Lorem Ipsum Lorem Ipsum is simply dummy text', Amount: '200$', AvailableBalance: '120$' },
           { Date: '04-13-2021', Description: 'What is Lorem Ipsum Lorem Ipsum is simply dummy text', Amount: '200$', AvailableBalance: '120$' },
@@ -18,13 +24,13 @@ class UserTransactions extends Component {
     }
  }
  renderTableHeader() {
-  let header = Object.keys(this.state.users[0])
+  let header = Object.keys(this.state.transactions[0])
   return header.map((key, index) => {
      return <th key={index}>{key.toUpperCase()}</th>
   })
 }
  renderTableData() {
-  return this.state.users.map((user, index) => {
+  return this.state.transactions.map((user, index) => {
      const { Date, Description, Amount, AvailableBalance } = user //destructuring
      return (
         <tr key={Date}>
@@ -38,20 +44,64 @@ class UserTransactions extends Component {
 }
 
   state = {};
-  render() {
+
+async componentDidMount() {
+	// Get account selected from accounts page
 	var { account="" } = this.props.location.state || {};
-	let accountNameHidden = "XXXX" + (account).toString().slice(4);
-	if(accountNameHidden === ""){
-		// TODO: Replace with primary checking account
-		accountNameHidden = "Primary Checking";
+
+	//console.log("Selected Account", account);
+
+	// Get current user account details
+	const user = auth.getCurrentUser();
+	let userAccountDetails = await getUserDetails(user._id);
+
+	// Default to the first available account, if not selected
+	if(account === ""){
+		//console.log("No Accounts selected");
+		// Select Checking account if available
+		if(userAccountDetails.data.checkingAccount){
+			account = userAccountDetails.data.checkingAccount.accountNumber;
+		}else if (userAccountDetails.data.savingAccount){
+			account = userAccountDetails.data.savingAccount.accountNumber;
+		} else {
+			// No accounts. Redirect to the Accounts page
+			//console.log("No Accounts");
+			return <Redirect to="accounts" />
+		}
+		console.log("Defaulting to account", account);
 	}
+
+	let activeAccount = {...this.state.activeAccount};
+	activeAccount = account;
+
+	let accountBalance = {...this.state.accountBalance};
+	if(userAccountDetails.data.checkingAccount && userAccountDetails.data.checkingAccount.accountNumber===account){
+		accountBalance = userAccountDetails.data.checkingAccount.balance;
+	}else if(userAccountDetails.data.savingAccount){
+		accountBalance = userAccountDetails.data.savingAccount.balance;
+	}else{
+		accountBalance = 0;
+	}
+
+	const transactionHistory = [...this.state.transactions];
+
+	// Fetch the transaction history
+	//getTransactionHistory(account);
+
+	this.setState({activeAccount, accountBalance, transactionHistory});
+}
+
+  render() {
+	console.log("Rendering account", this.state.activeAccount);
+	let accountNameHidden = "XXXX" + (this.state.activeAccount).toString().slice(4);
+
     return (
     <div className="container">
       <div className="row justify-content-center">
           <h1 className="mt-4 mb-4">Account {accountNameHidden}</h1>
       </div>
       <h3>Summary</h3>
-      <h2>Available Balance (as of today):  900$</h2><br></br>
+      <h2>Available Balance (as of today): ${this.state.accountBalance}</h2><br></br>
       <form action="/" method="get">
         <label htmlFor="header-search">
             <span className="visually-hidden">Search UserTransactions : -</span>
