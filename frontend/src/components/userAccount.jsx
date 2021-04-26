00d264b51e5c9ede7c69fef06af1a9a36d223cb8
Sagnik Chowdhury <sagnik.chowdhury@sjsu.edu>
2021-04-26 01:22:10 +0000

    Stub version of user accounts list

--- frontend/src/components/userAccount.jsx
+++ frontend/src/components/userAccount.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
 
 class UserAccount extends Component {
-  state = {};
-  render() {
-    return <h1>User - userAccount</h1>;
+  constructor(props) {
+	super(props);
+	this.state = {
+		users: [
+			{ Name: "XXXXXX1234", Type: "Checking", Balance: "$100" },
+			{ Name: "XXXXXX2345", Type: "Savings", Balance: "$500" },
+		]
+	}
   }
+
+	renderTableHeader(){
+		let header = Object.keys(this.state.users[0])
+		return header.map((key, index) => {
+			return <th key={index}>{key.toUpperCase()}</th>
+		})
+	}
+
+	renderTableData() {
+		return this.state.users.map((user, index) => {
+			const { Name, Type, Balance } = user
+			return (
+				<tr key={Name}>
+					<td>{Name}</td>
+					<td>{Type}</td>
+					<td>{Balance}</td>
+				</tr>
+			)
+		})
+	}
+
+	state = {};
+	render() {
+		return (
+			<div className="container">
+				<h3>Accounts</h3>
+				<div>
+					<table id="users">
+						<tbody>
+						<tr>{this.renderTableHeader()}</tr>
+							{this.renderTableData()}
+						</tbody>
+					</table> 
+				</div>
+			</div>
+		)
+	};
 }
 
 export default UserAccount;
