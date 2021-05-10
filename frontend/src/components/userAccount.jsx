8a3a7247bd1ed811a7f86f797bf7d0f42d5f5365
ChandaluriHimaja <himaja.chandaluri@sjsu.edu>
2021-05-10 10:49:53 -0700

    Handling connection between accounts page and transactions page

--- frontend/src/components/userAccount.jsx
+++ frontend/src/components/userAccount.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class UserAccount extends Component {
   state = {};
   render() {
-    return <h1>User - userAccount</h1>;
+    return (
+      <React.Fragment>
+        <h1>User - userAccount</h1>
+        <Link
+          to={{
+            pathname: "transactions",
+            state: {
+              account: "CheckingAccount",
+            },
+          }}
+        >
+          link
+        </Link>
+      </React.Fragment>
+    );
   }
 }
 
